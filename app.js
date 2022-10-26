f24a9b95f71e85effe9a1cb2d05cc4474b629172
tuilahienne <hientranvan27@gmail.com>
2022-10-26 16:03:03 +0700

    change config deloy vercel

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
+const debug = require("debug")("myphamnhanhon-server:server");
+const http = require("http");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
 const app = express();
+const server = http.createServer(app);
+
+dotenv.config();
 
 app.use(cors());
 app.use(logger("dev"));
@@ -17,5 +25,24 @@ app.get("/", (req, res) => {
     res.status(200).json({ status: 404, message: "NOT FOUND" });
 });
 app.use("/api", require("./src/routes"));
+
+const port = process.env.PORT || "3000";
+app.set("port", port);
+
+mongoose
+    .connect(process.env.MONGODB_CONNECT_URL)
+    .then(() => {
+        server.listen(port);
+        console.log(`Server run port: ${port}`);
+        // server.on("error", onError);
+        // server.on("listening", onListening);
+        console.log(
+            `Server connect success ${process.env.MONGODB_CONNECT_URL}`
+        );
+    })
+    .catch((err) => {
+        console.log({ message: `Connect fail`, error: err });
+        process.exit(1);
+    });
 
 module.exports = app;
