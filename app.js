bb52cef05f065c6a6d9bedb17f25e7ecf85eef18
tuilahienne <hientranvan27@gmail.com>
2022-10-26 15:04:37 +0700

    add default page index

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+    res.status(200).json({ status: 404, message: "NOT FOUND" });
+});
 app.use("/api", require("./src/routes"));
 
 module.exports = app;
