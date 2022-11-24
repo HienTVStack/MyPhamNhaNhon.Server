824e6be0a3850698b5ad57067b585233ce1ccaac
tuilahienne <hientranvan27@gmail.com>
2022-11-24 15:17:17 +0700

    Check code when create discount voucher

--- src/routes/discountRoute.js
+++ src/routes/discountRoute.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const { body } = require("express-validator");
 const DisCountController = require("../controllers/DiscountController");
+const Discount = require("../models/Discount");
+const validate = require("../handlers/validation");
 const router = express.Router();
 
 router.get("/getAll", DisCountController.getAll);
-router.post("/create", DisCountController.create);
+router.post(
+    "/create",
+    body("code").custom(async (value) => {
+        return await Discount.findOne({ code: value }).then((discount) => {
+            if (discount) {
+                return Promise.reject(`Mã khuyến mãi đã được sử dụng`);
+            }
+        });
+    }),
+    validate.validation,
+    DisCountController.create
+);
 router.post("/checkCodeByCustomer", DisCountController.checkCodeByCustomer);
 
 module.exports = router;
